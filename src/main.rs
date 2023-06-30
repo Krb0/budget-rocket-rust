4662d18cfbcf37cbff73d13504b731eb57747106
Krb0 <maurodavidcaceres@gmail.com>
2023-06-30 00:23:42 -0300

    feat: :sparkles: initial SeaORM & db setup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+mod database;
 mod routes;
 use routes::auth::auth_router;
-#[macro_use] extern crate rocket;
-
+#[macro_use]
+extern crate rocket;
+use database::setup::set_up_db;
 #[launch]
-fn rocket() -> _ {
+async fn rocket() -> _ {
+    let db = match set_up_db().await {
+        Ok(db) => db,
+        Err(err) => panic!("{}", err),
+    };
+    println!("Database setup complete! ");
 
     rocket::build().mount("/auth", auth_router())
-
 }
